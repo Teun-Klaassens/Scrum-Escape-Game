5d3ab66c58d58c468db765d614d717276909896e
Teun Klaassens <w.t.klaassens@student.tudelft.nl>
2025-05-09 11:03:44 +0200

    Alle kamers gemaakt; hele simpele implementatie

---
+++ src/main/java/org/scrumEscape/base/Kamers/DailyScrum.java
@@ -0,0 +1,18 @@
+package org.scrumEscape.base.Kamers;
+
+import org.scrumEscape.base.Kamer;
+import org.scrumEscape.classes.Player;
+import java.util.ArrayList;
+
+public class DailyScrum extends Kamer {
+    ArrayList<Player> teamleden = new ArrayList<>();
+
+    void roepen(){
+        System.out.println("Vertraging");
+    }
+
+    @Override
+    public String getBeschrijving() {
+        return "Je krijgt een lijst teamleden en moet aangeven wie welke status-update zou geven. Een vergeten update roept het monster “Vertraging” op.";
+    }
+}
---
+++ src/main/java/org/scrumEscape/base/Kamers/TIA.java
@@ -0,0 +1,15 @@
+package org.scrumEscape.base.Kamers;
+
+import org.scrumEscape.base.Kamer;
+
+public class TIA extends Kamer {
+
+    public TIA() {
+    }
+
+    @Override
+    public String getBeschrijving() {
+        // TIA = Transparency, Inspection, Adaptation.
+        return "In deze kamer het eindspel. Pas als je hier doorheen komt, heb je gewonnen! Zoek uit wat TIA is!";
+    }
+}
