4a572d3f39f724d9d97e2310d418fdc40f4ed993
Roody <roodyalexis36@gmail.com>
2025-05-12 11:16:00 +0200

    Implemented Kamer Class

---
+++ src/main/java/org/scrumEscape/classes/Taak.java
@@ -0,0 +1,4 @@
+package org.scrumEscape.classes;
+
+public class Taak {
+}
---
+++ src/main/java/org/scrumEscape/interfaces/IKamerState.java
@@ -0,0 +1,11 @@
+package org.scrumEscape.interfaces;
+
+public class IKamerState {
+	public final int behaaldTaken;
+	public final int aantalTaken;
+
+	IKamerState(int behaaldTaken, int aantalTaken) {
+		this.behaaldTaken = behaaldTaken;
+		this.aantalTaken = aantalTaken;
+	}
+}
